1be51ec7ca2f55084cde054f66cc61f087459a07
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-16 14:57:37 -0300

    database connect log

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,4 +2,13 @@ import pkg from '@prisma/client';
 
 const { PrismaClient } = pkg;
 const db = new PrismaClient();
+
+connect();
+
 export default db;
+
+async function connect() {
+    try {
+        await db.$connect(), console.log(`Connected to database ${new Date()}`);
+    } catch (err) { console.error(`\n\nError connecting to database:\n\n${err}\n\n${new Date()}\n\n`) };
+};
